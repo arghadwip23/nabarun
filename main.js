6c69fa6e52b714c6c739ee54c39783ba570241fc
arghadwip23 <arghadwip23@gmail.com>
2021-11-22 11:11:08 +0530

    update

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 console.log('Hello World!');
 const burst = new mojs.Burst({
-  radius:{0:100}
+  radius: { 0: 100 },
+    count: 10,
+    degree: 360,
+    children: {
+      fill: { 'red': 'red' },
+      duration: 1000,
+      radius: 10
+    }
+});
+
+document.addEventListener('click', function(e) {
+  burst.replay();
 });
 anime({
   loop:true,
@@ -25,7 +36,3 @@ heart.addEventListener("click" , (e)=>{
    } , 1000);
 
 });
-
-ScreenS.addEventListener("click" , ()=>{
-  brust.replay();
-})
