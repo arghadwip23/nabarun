079ddd38f48308893441e74278b743e42c442593
arghadwip23 <arghadwip23@gmail.com>
2021-11-23 13:15:58 +0530

    Vab

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 console.log('Hello World!');
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBbH3gIASwYSkRMcOhKZ5oHXH7QPTNf3FE",
+    authDomain: "hbdsrmb.firebaseapp.com",
+    projectId: "hbdsrmb",
+    storageBucket: "hbdsrmb.appspot.com",
+    messagingSenderId: "573945335907",
+    appId: "1:573945335907:web:2aaaab3fb39e827f77e856",
+    measurementId: "G-K3WKTK72FV"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+
 const burst = new mojs.Burst({
   radius: { 0: 100 },
     count: 10,
